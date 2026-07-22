e4854f05b898efd2a82a3962279d5bca03c5897f
David Swygart <davidswygart@gmail.com>
2026-07-22 11:52:17 -0400

    Increase MTU target
    also increase data length, but I don't think it does anything

--- ble_test.ino
+++ ble_test.ino
@@ -5,7 +5,7 @@
 #define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
-#define TARGET_MTU 256
+#define TARGET_MTU 512
 
 
 NimBLEServer *pServer = NULL;
@@ -51,6 +51,7 @@ class MyServerCallbacks: public NimBLEServerCallbacks {
         if (pClient != nullptr) {
             pClient->exchangeMTU(); 
         } 
+        pServer->setDataLen(connInfo.getConnHandle(), 251);
     }
     
     void onDisconnect(NimBLEServer* pServer, NimBLEConnInfo& connInfo, int reason) override {
